5a1c5a69c3554daeacb70b8a21c7583538fa42d4
realJimboSlice <jimm7493@stud.kea.dk>
2024-08-07 18:40:28 +0200

    Updated report page to redirect correctly, debugged and tested both 500 and 404 to ensure it'll catch both

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const tags = [
   "wcag2a",
@@ -21,47 +21,45 @@ async function fetchData(params) {
     );
     if (!response.ok) {
       if (response.status === 404) {
-        notFound(); // Handle 404 errors
+        // Return error 404
+        return { errorCode: 404 };
       }
       throw new Error("Network response was not ok");
     }
     return await response.json();
   } catch (error) {
     console.error("Failed to fetch data:", error);
-    throw new Error("Failed to fetch data.");
+    // Return error 500
+    return { errorCode: 500 };
   }
 }
 
 export default async function ReportPage({ searchParams }) {
   const params = new URLSearchParams(searchParams);
+  const data = await fetchData(params);
 
-  try {
-    const data = await fetchData(params);
-
-    if (!data) {
-      throw new Error("No data returned");
-    }
-
-    return (
-      <main>
-        <h1>Report for {data.url}</h1>
-        <p>Found {data.violations.length} issues</p>
-        <Image
-          alt={`Screenshot of ${data.url}`}
-          src={data.screenshot.url}
-          width={data.screenshot.width}
-          height={data.screenshot.height}
-        />
-      </main>
-    );
-  } catch (error) {
-    console.error("Error in ReportPage:", error);
-    // Redirect to custom 500 page for server-side errors
-    if (error.message === "Failed to fetch data.") {
-      redirect("/errors/error-500");
-    } else {
-      notFound();
-    }
-    return null;
+  if (data.errorCode === 404) {
+    redirect("/errors/error-404"); // 404 Not Found
   }
+
+  if (data.errorCode === 500) {
+    redirect("/errors/error-500"); // 500 Internal Server Error
+  }
+
+  if (!data) {
+    redirect("/errors/error-500"); // 500 Internal Server Error fallback
+  }
+
+  return (
+    <main>
+      <h1>Report for {data.url}</h1>
+      <p>Found {data.violations.length} issues</p>
+      <Image
+        alt={`Screenshot of ${data.url}`}
+        src={data.screenshot.url}
+        width={data.screenshot.width}
+        height={data.screenshot.height}
+      />
+    </main>
+  );
 }
